9a08f5d33ab90450388d47b1036aa2308f6a8174
Renoir Lucena <renoirlucena@gmail.com>
2023-11-08 13:10:01 -0600

    fixed eerrors and updated code

--- csv-transform.py
+++ csv-transform.py
@@ -1,72 +1,78 @@
 import csv
+import re
 
-def load_ddd_data(file_path):
-    """
-    Loads the DDD data from a CSV file and returns a dictionary with the city-state tuple as the key and the DDD as the value.
-    :param file_path: str
-    :return: dict
-    """
+def load_ddd_data(ddd_data_path):
     ddd_dict = {}
-    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        headers = next(reader, None)  # Read the header row
-        headers = [h.strip() for h in headers]  # Strip spaces from header names
-        for row in csv.DictReader(file, fieldnames=headers):
-            # Create a tuple of city and state as key, and DDD as the value
-            ddd_dict[(row['city'].strip().lower(), row['state'].strip().upper())] = row['ddd'].strip()
+    with open(ddd_data_path, mode='r', encoding='utf-8') as ddd_file:
+        ddd_reader = csv.DictReader(ddd_file, delimiter=',')
+        ddd_reader.fieldnames = [name.strip() for name in ddd_reader.fieldnames]  # Clean header names
+        for row in ddd_reader:
+            key = (row['city'].strip().lower(), row['state'].strip().upper())
+            ddd_dict[key] = row['ddd'].strip()
     return ddd_dict
 
-def process_cellular(number, city, state, ddd_dict):
-    """
-    Processes the cellular number to ensure it has the correct DDD prefix based on the city and state, if necessary.
-    :param number: str
-    :param city: str
-    :param state: str
-    :param ddd_dict: dict
-    :return: str
-    """
-    # Check if DDD is missing and add it if necessary
-    if len(number) == 9:
-        city_state_tuple = (city.strip().lower(), state.strip().upper())
-        ddd = ddd_dict.get(city_state_tuple)
-        if ddd:
-            return f'{ddd}{number}'
-    return number
+def format_phone_number(phone, ddd_dict, city, state):
+    phone = re.sub(r'\D', '', phone)  # Remove non-digit characters
+    if len(phone) == 9 and phone.startswith('9'):
+        key = (city.lower(), state.upper())
+        ddd = ddd_dict.get(key, '')
+        phone = '55' + ddd + phone
+    return phone
 
-def transform_csv(input_path, output_path, ddd_data_path):
-    """
-    Transforms the input CSV file to add missing DDDs to phone numbers and saves the result to the output path.
-    :param input_path: str
-    :param output_path: str
-    :param ddd_data_path: str
-    """
-    # Load DDD data
+def format_name(name):
+    if not name:  # Check if the name is None or empty string
+        return ''
+    return ' '.join(word.capitalize() for word in name.split())
+
+
+def transform_csv(input_csv_path, output_csv_path, ddd_data_path):
     ddd_dict = load_ddd_data(ddd_data_path)
+    written_rows = set()  # Set to store unique identifiers of written rows
 
-    with open(input_path, mode='r', newline='', encoding='utf-8') as infile, \
-         open(output_path, mode='w', newline='', encoding='utf-8') as outfile:
-        reader = csv.DictReader(infile)
-        fieldnames = ['mobile', 'firstName', 'lastName', 'displayName']
-        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    with open(input_csv_path, mode='r', encoding='utf-8') as infile, \
+         open(output_csv_path, mode='w', encoding='utf-8', newline='') as outfile:
 
+        reader = csv.DictReader(infile, delimiter=';')
+        fieldnames = ['mobile', 'firstname', 'lastname', 'display_name']
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter=';')
         writer.writeheader()
 
         for row in reader:
-            # Process cellular number
-            processed_cellular = process_cellular(row['customer_cellular'], row['customer_city'], row['customer_state'], ddd_dict)
-            # Write the transformed data
-            writer.writerow({
-                'mobile': processed_cellular,
-                'firstName': row['first_name'].strip(),
-                'lastName': row['last_name'].strip(),
-                'displayName': f"{row['first_name'].strip()} {row['last_name'].strip()}"
-            })
+            phone = row.get('customer_cellular', '').strip()
 
-if __name__ == "__main__":
-    # Define paths to the files
-    input_csv_path = '/Users/renoirlucena/code/csv-transform/crm_export.csv'
-    output_csv_path = '/Users/renoirlucena/code/csv-transform/processed_crm_export.csv'  # Name the output file as processed_crm_export.csv
-    ddd_csv_path = '/Users/renoirlucena/code/csv-transform/ddd_data.csv'
+            # If there's no phone number or if the phone number is already written, skip the row
+            if not phone or phone in written_rows:
+                continue
 
-    # Run the transformation
-    transform_csv(input_csv_path, output_csv_path, ddd_csv_path)
+            formatted_row = {}  # Create a new dictionary for the formatted data
+
+            # Format the name and create display name
+            firstname = format_name(row.get('customer_name', ''))
+            lastname = format_name(row.get('customer_lastname', ''))
+            formatted_row['firstname'] = firstname
+            formatted_row['lastname'] = lastname
+            formatted_row['display_name'] = firstname + ' ' + lastname
+
+            # Format phone numbers
+            city = row.get('customer_city', '')
+            state = row.get('customer_state', '')
+            formatted_phone = format_phone_number(phone, ddd_dict, city, state)
+
+            # If the formatted phone is already in written_rows, skip the row
+            if formatted_phone in written_rows:
+                continue
+
+            formatted_row['mobile'] = formatted_phone
+
+            # Add the phone to the set of written rows
+            written_rows.add(formatted_phone)
+
+            writer.writerow(formatted_row)
+
+# Paths to the data
+input_csv_path = 'data/input_data.csv'
+output_csv_path = 'data/output_data.csv'
+ddd_data_path = 'data/ddd_data.csv'
+
+# Run the CSV transformation
+transform_csv(input_csv_path, output_csv_path, ddd_data_path)
